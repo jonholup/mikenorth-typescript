b98a218fa94f00c0914bac444d45a27539360090
Jonny Holupchinski <jonholup@gmail.com>
2017-09-15 09:30:18 -0500

    1st day complete

---
+++ exercises/dealer/src/dealer.ts
@@ -0,0 +1,67 @@
+/**
+ * Shuffle an array in place
+ * @param a Array to shuffle
+ */
+function shuffleArray(a: any[]) {
+  // Iterate over the array
+  for (let i = a.length; i; i--) {
+    // Get next index
+    let j = Math.floor(Math.random() * i);
+    // Swap positions
+    [a[i - 1], a[j]] = [a[j], a[i - 1]];
+  }
+}
+
+export enum Suit {
+  Clubs,
+  Diamonds,
+  Hearts,
+  Spades
+}
+
+export enum CardNumber {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King
+}
+
+export type Card = [Suit, CardNumber];
+
+export class Dealer {
+  cards: Card[] = new Array();
+  constructor() {
+    this.setupDeck();
+    this.shuffle();
+  }
+  setupDeck() {
+    for (let i = 0; i < Object.keys(CardNumber).length / 2; i++) {
+      for (let j = 0; j < Object.keys(Suit).length / 2; j++) {
+        this.cards.push([j, i]);
+      }
+    }
+  }
+  getLength() {
+    return this.cards.length;
+  }
+  shuffle() {
+    shuffleArray(this.cards);
+  }
+  dealHand(numCards = 5) {
+    if (numCards > this.getLength()) throw new Error('Not enough cards left!');
+    if (numCards < 0) throw new Error('You have to give me your cards now');
+    return this.cards.splice(this.cards.length - numCards, numCards);
+  }
+  readCard(card: Card) {
+    return `${CardNumber[card[1]]} of ${Suit[card[0]]}`;
+  }
+}
---
+++ exercises/stack/src/stack.ts
@@ -0,0 +1,51 @@
+interface IStack<T> {
+  push(item: T): IStack<T>;
+  push(items: T[]): IStack<T>;
+  pop(): T | undefined;
+  length(): number;
+  print(): void;
+}
+
+interface ListNode<T> {
+  data: T;
+  next?: ListNode<T>;
+}
+
+export class Stack<T> implements IStack<T> {
+  private head: ListNode<T> | undefined;
+  push(item: T): Stack<T>;
+  push(items: T[]): Stack<T>;
+  push(arg: T[] | T) {
+    if (arg instanceof Array) {
+      arg.forEach(item => this.push(item));
+    } else {
+      let newNode = {
+        data: arg,
+        next: this.head
+      };
+      this.head = newNode;
+    }
+    return this;
+  }
+  pop() {
+    let n = this.head;
+    this.head = this.head ? this.head.next : undefined;
+    return n ? n.data : undefined;
+  }
+  length() {
+    let i = 0;
+    let n: ListNode<T> | undefined = this.head;
+    while (n) {
+      i++;
+      n = n.next;
+    }
+    return i;
+  }
+  print() {
+    let n: ListNode<T> | undefined = this.head;
+    while (n) {
+      console.log(n.data);
+      n = n.next;
+    }
+  }
+}
